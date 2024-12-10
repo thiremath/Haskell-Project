2c6e79eb9b9b0c37283f019d305430b807b60fac
Tejas Hiremath <tejashiremath@Tejass-MacBook-Pro.local>
2024-12-09 20:48:24 -0500

    Implemented AST and Bank Files.

--- ast/ast.hs
+++ ast/ast.hs
@@ -1,10 +1,21 @@
+-- Name - Tejas Hiremath
+-- Project 6
+
 module Ast where
 
 data Expr = Plus Expr Expr | Minus Expr Expr | Times Expr Expr | Div Expr Expr
     | Literal Float
 
 eval :: Expr -> Float
-eval = ???
+eval (Literal x) = x
+eval (Plus e1 e2) = eval e1 + eval e2
+eval (Minus e1 e2) = eval e1 - eval e2
+eval (Times e1 e2) = eval e1 * eval e2
+eval (Div e1 e2) = 
+    let denominator = eval e2
+    in if denominator == 0
+        then error "Division By Zero Error"
+        else eval e1 / denominator
 
 -- Should eval to "5.0"
 test1 = Plus (Literal 3.0) (Literal 2.0)
@@ -14,4 +25,8 @@ test2 = Plus (Literal 3.0) (Div (Literal 1.0) (Literal 2.0))
 
 -- Should eval to "15.5"
 test3 = Plus (Times (Literal 3.0) (Literal 5.0)) (Div (Literal 1.0) (Literal 2.0))
+
+test4 =
+    let input1 = Plus (Literal 5.0) (Div (Literal 1.0) (Literal 2.0)) in
+    eval input1
 
--- bank/bank.hs
+++ bank/bank.hs
@@ -1,12 +1,49 @@
+-- Name - Tejas Hiremath
+-- Project 6
+
 module Bank where
 
-newtype BankOp a = ???
+-- Define BankOp as a wrapper for a state transition function
+newtype BankOp a = BankOp (Float -> (a, Float))
 
+-- Functor instance for BankOp
+instance Functor BankOp where
+    fmap f (BankOp g) = BankOp (\state -> 
+        let (result, newState) = g state 
+        in (f result, newState))
+
+-- Applicative instance for BankOp
+instance Applicative BankOp where
+    pure x = BankOp (\state -> (x, state))
+    BankOp f <*> BankOp x = BankOp (\state -> 
+        let (func, state') = f state
+            (result, state'') = x state'
+        in (func result, state''))
+
+-- Monad instance for BankOp to enable chaining
+instance Monad BankOp where
+    return = pure
+    BankOp action >>= next = BankOp (\state -> 
+        let (result, state') = action state
+            BankOp nextAction = next result
+        in nextAction state')
+
+-- Deposit: Add a specified amount to the balance
 deposit :: Float -> BankOp ()
+deposit amount = BankOp (\balance -> ((), balance + amount))
 
+-- Withdraw: Subtract an amount, allowing up to $100 overdraft
 withdraw :: Float -> BankOp Float
+withdraw amount = BankOp (\balance -> 
+    let actualWithdrawal = if balance - amount >= -100 
+                           then amount 
+                           else balance + 100
+    in (actualWithdrawal, balance - actualWithdrawal))
 
+-- GetBalance: Retrieve the current balance
+getBalance :: BankOp Float
+getBalance = BankOp (\balance -> (balance, balance))
+
+-- Run a BankOp operation starting with an initial balance of 0
 runBankOp :: BankOp a -> a
-
-
-
+runBankOp (BankOp op) = fst (op 0)
